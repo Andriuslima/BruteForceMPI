d4315f777738796bca5eee55eb5de60b2d2d4097
Andriuslima <lima.andrius07@gmail.com>
2018-06-20 18:24:52 -0300

    Testes corrigidos

--- brute_force.py
+++ brute_force.py
@@ -1,22 +1,7 @@
 from mpi4py import MPI
 import numpy
 
-class BruteForce():
-
-    """
-    password_size = int: Max size of password
-    numbers = bool: true if numbers are accepted
-    letters = bool: true if letters are accepted
-    special_char = bool: true if special characters are accepted
-
-    Return: ???
-    """
-    def number_possi(self, max_password_size, numbers, letters, special_char):
-        return None
-
-    def force_password(self, range, real_password):
-
-
+# EXECUTE = mpiexec -n 3 python brute_force.py
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
@@ -29,7 +14,9 @@ if rank == 0:
     comm.send(data, dest=1, tag=11)
 elif rank == 1:
     data = comm.recv(source=0, tag=11)
+    print("I'm ranked 1 and I recieved this data: " + data)
 
+"""
 # non blocking communation
 if rank == 0:
     data = {'a': 7, 'b': 3.14}
@@ -60,3 +47,4 @@ elif rank == 1:
 
 ## Collective Communication -> Broadcast
 # http://mpi4py.scipy.org/docs/usrman/tutorial.html
+"""
