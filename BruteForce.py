1ccc8a0035e4c164ba1200e1c010757e2ea4f233
Andrius Lima <andriuslima@outlook.com.br>
2018-06-29 21:07:52 -0300

    Rodando em n threads passando mensagem entre as threads

--- BruteForce.py
+++ BruteForce.py
@@ -3,19 +3,39 @@ import math
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
-cores = comm.Get_size()
+size = comm.Get_size()
 SENHA = "9999"
 
-words = [w.strip() for w in open("wordlist.txt", "r").readlines()]
-n = len(words)
-m = math.ceil(n/cores)
+if rank == 0:
+    i = 1
+    j = 0
+    # Fica recebendo mensagens das threads para saber qual achou a senha
+    while True:
+        recv = comm.recv(source=i)
+        if recv['PasswordFound']: break
 
-if rank == cores-1: search_range = range(m*rank, n)
-else: search_range = range( m*rank, (m*rank) + m )
+        if i < size-1: i += 1
+        else: i = 1
+        j += 1
 
-data = {'PasswordFound': False, 'Password': ''}
-for i in search_range:
-    if SENHA == words[i]:
-        data['PasswordFound'] = True
-        data['Password'] = words[i]
-        print("THREAD " + str(rank) + ": found password")
+    print("Thread " + str(i) + " found password")
+
+else:
+    size -= 1
+    rank -= 1
+    words = [w.strip() for w in open("wordlist.txt", "r").readlines()]
+    n = len(words)
+    m = math.ceil(n / size)
+
+    if rank == size-1: search_range = range(m * rank, n)
+    else: search_range = range( m*rank, (m*rank) + m )
+
+    data = {'PasswordFound': False, 'Password': ''}
+    for i in search_range:
+        if SENHA == words[i]:
+            data['PasswordFound'] = True
+            data['Password'] = words[i]
+            comm.send(data, dest=0)
+            break
+
+        comm.send(data, dest=0)
