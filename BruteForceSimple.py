f2aefd60ed79b7d572f3b7b94a069a30da3f2300
drumm100 <marcelo_drumm@hotmail.com>
2018-07-01 15:14:39 -0300

    Adicionado BruteForceSimple e wordlists com 6 dígigos.

--- BruteForceSimple.py
+++ BruteForceSimple.py
@@ -1,26 +1,36 @@
-import requests
-import sys
-
+#import requests
+#import sys
+import time
 
 G_usuario = "Teste"
-G_senha = "5567"
+G_senha = "99999999"
+start = time.time()
 
 
 def brute(usuario, senha):
     if (G_usuario == usuario):
         if (G_senha == senha):
             print("Senha encontrada: ", senha)
+            tempo_execucao = time.time() - start
+
+            #a = time.strftime("%H:%M:%S", time.gmtime(tempo_execucao))
+            print("Tempo de Execução: ", tempo_execucao)
             return True
         else:
-            #print("Tentantiva falha: ", senha)
+            #print("Tentantiva falha:", senha)
             return False   
 
-
 def main():
-	words = [w.strip() for w in open("wordlist.txt", "rb").readlines()] 
-	for password in words:
-		brute(G_usuario,password)
+    #words = [w.strip() for w in open("wordlist4.txt", "r").readlines()]
+
+
+    with open("wordlist4.txt") as infile:
+        for password in infile:
+            password = password.strip()
+            #brute(G_usuario,password)
+            if brute(G_usuario,password) == True:
+                break
 
 
 if __name__ == '__main__':
-	main()
+    main()
